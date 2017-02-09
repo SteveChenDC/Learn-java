97811c23d1f8351f368b5d356d7b9a672322934d
Steve Chen <stevechen@CHIC02SV0F4H03Q.grubhub.local>
2017-02-09 10:39:41 -0500

    Add .gitign ore to ignore .metadata and .recommenders

--- quickdip/src/quickdip/Execise1b.java
+++ quickdip/src/quickdip/Execise1b.java
@@ -1,9 +1,10 @@
 package quickdip;
 
-class Execise1b {
+public class Execise1b {
 	public static void main(String [] args){
 		int x = 1;
 		while (x < 10) {
+		x = x + 1;
 			if ( x > 3 ) {
 				System.out.println("big x");
 			}
