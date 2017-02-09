dd3857fac62970b15261c5d7848336dd6d68e3cf
Steve Chen <stevechen@CHIC02SV0F4H03Q.grubhub.local>
2017-02-09 10:50:11 -0500

    Add corrected exercise b

--- quickdip/src/quickdip/ExerciseB.java
+++ quickdip/src/quickdip/ExerciseB.java
@@ -1,11 +1,13 @@
 package quickdip;
 
-public static void main(String [] args) {
-	int x = 5;
-	while (x > 1){
-		x = x -1;
-		if (x < 3) {
-			System.out.println("small x");
+public class ExerciseB{
+	public static void main(String [] args) {
+		int x = 5;
+		while (x > 1){
+			x = x - 1;
+			if (x < 3) {
+				System.out.println("small x");
+			}
 		}
 	}
 }
