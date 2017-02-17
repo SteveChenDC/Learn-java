b94c11fcc952d8f1cd72c05cc27b6c6ad197de51
Steve Chen <stevechen@CHIC02SV0F4H03Q.grubhub.local>
2017-02-17 12:05:14 -0500

    Add first JFrame GUI

---
+++ FirstGUI/src/SimpleGui1.java
@@ -0,0 +1,20 @@
+/**
+ * Created by stevechen on 2/17/17.
+ */
+import javax.swing.*;
+
+
+public class SimpleGui1 {
+    public static void main (String[] args){
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click me");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.getContentPane().add(button);
+
+        frame.setSize(300,300);
+
+        frame.setVisible(true);
+    }
+}
